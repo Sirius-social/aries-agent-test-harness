6752e65be1543c6285e607c3143bccbc19fa9086
Mikhail Lytaev <mikelytaev@gmail.com>
2021-10-21 01:29:10 +0300

    indilynx python bc

--- aries-backchannels/indiLynxPython/indilynx_backchannel.py
+++ aries-backchannels/indiLynxPython/indilynx_backchannel.py
@@ -32,6 +32,7 @@ from python.storage import (
     pop_resource_latest,
 )
 import sirius_sdk
+from helpers import get_agent_params
 
 # from helpers.jsonmapper.json_mapper import JsonMapper
 
@@ -844,126 +845,6 @@ class IndiLynxCloudAgentBackchannel(AgentBackchannel):
 
         return (501, "501: Not Implemented\n\n".encode("utf8"))
 
-    def _process(self, args, env, loop):
-        proc = subprocess.Popen(
-            args,
-            env=env,
-            encoding="utf-8",
-        )
-        loop.run_in_executor(
-            None,
-            output_reader,
-            proc.stdout,
-            functools.partial(self.handle_output, source="stdout"),
-        )
-        loop.run_in_executor(
-            None,
-            output_reader,
-            proc.stderr,
-            functools.partial(self.handle_output, source="stderr"),
-        )
-        return proc
-
-    def get_process_args(self, bin_path: str = None):
-        # TODO aca-py needs to be in the path so no need to give it a cmd_path
-        cmd_path = "aca-py"
-        if bin_path is None:
-            bin_path = DEFAULT_BIN_PATH
-        if bin_path:
-            cmd_path = os.path.join(bin_path, cmd_path)
-        print("Location of ACA-Py: " + cmd_path)
-        if self.get_acapy_version_as_float() > 56:
-            return list(flatten(([cmd_path, "start"], self.get_agent_args())))
-        else:
-            return list(
-                flatten((["python3", cmd_path, "start"], self.get_agent_args()))
-            )
-
-    async def detect_process(self):
-        # text = None
-
-        async def fetch_swagger(url: str, timeout: float):
-            text = None
-            start = default_timer()
-            async with ClientSession(timeout=ClientTimeout(total=3.0)) as session:
-                while default_timer() - start < timeout:
-                    try:
-                        async with session.get(url) as resp:
-                            if resp.status == 200:
-                                text = await resp.text()
-                                break
-                    except (ClientError, asyncio.TimeoutError):
-                        pass
-                    await asyncio.sleep(0.5)
-            return text
-
-        status_url = self.admin_url + "/status"
-        status_text = await fetch_swagger(status_url, START_TIMEOUT)
-        print("Agent running with admin url", self.admin_url)
-
-        if not status_text:
-            raise Exception(
-                "Timed out waiting for agent process to start. "
-                + f"Admin URL: {status_url}"
-            )
-        ok = False
-        try:
-            status = json.loads(status_text)
-            ok = isinstance(status, dict) and "version" in status
-            if ok:
-                self.acapy_version = status["version"]
-                print(
-                    "ACA-py Backchannel running with ACA-py version:",
-                    self.acapy_version,
-                )
-        except json.JSONDecodeError:
-            pass
-        if not ok:
-            raise Exception(
-                f"Unexpected response from agent process. Admin URL: {status_url}"
-            )
-
-    async def start_process(
-        self, python_path: str = None, bin_path: str = None, wait: bool = True
-    ):
-        my_env = os.environ.copy()
-        python_path = DEFAULT_PYTHON_PATH if python_path is None else python_path
-        if python_path:
-            my_env["PYTHONPATH"] = python_path
-
-        agent_args = self.get_process_args(bin_path)
-
-        # start agent sub-process
-        self.log(f"Starting agent sub-process ...")
-        self.log(f"agent starting with params: ")
-        self.log(agent_args)
-        loop = asyncio.get_event_loop()
-        self.proc = await loop.run_in_executor(
-            None, self._process, agent_args, my_env, loop
-        )
-        if wait:
-            await asyncio.sleep(1.0)
-            await self.detect_process()
-
-    def _terminate(self):
-        if self.proc and self.proc.poll() is None:
-            self.proc.terminate()
-            try:
-                self.proc.wait(timeout=0.5)
-                self.log(f"Exited with return code {self.proc.returncode}")
-            except subprocess.TimeoutExpired:
-                msg = "Process did not terminate in time"
-                self.log(msg)
-                raise Exception(msg)
-
-    async def terminate(self):
-        loop = asyncio.get_event_loop()
-        if self.proc:
-            await loop.run_in_executor(None, self._terminate)
-        await self.client_session.close()
-        if self.webhook_site:
-            await self.webhook_site.stop()
-
 
 async def main(start_port: int, show_timing: bool = False, interactive: bool = True):
 
@@ -973,19 +854,23 @@ async def main(start_port: int, show_timing: bool = False, interactive: bool = T
         print("Got extra args:", EXTRA_ARGS)
         extra_args = json.loads(EXTRA_ARGS)
 
-    genesis = await default_genesis_txns()
-    if not genesis:
-        print("Error retrieving ledger genesis transactions")
-        sys.exit(1)
+    # genesis = await default_genesis_txns()
+    # if not genesis:
+    #     print("Error retrieving ledger genesis transactions")
+    #     sys.exit(1)
 
     agent = None
 
     try:
+
+        gov_agent_params = await get_agent_params("agent1")
+        sirius_sdk.init(**gov_agent_params)
+
         agent = IndiLynxCloudAgentBackchannel(
             "indiLynx." + AGENT_NAME,
             start_port + 1,
             start_port + 2,
-            genesis_data=genesis,
+            genesis_data="{}",
             extra_args=extra_args,
         )
 
@@ -994,9 +879,9 @@ async def main(start_port: int, show_timing: bool = False, interactive: bool = T
 
         # start aca-py agent sub-process and listen for web hooks
         await agent.listen_webhooks(start_port + 3)
-        await agent.register_did()
+        #await agent.register_did()
 
-        await agent.start_process()
+        #await agent.start_process()
         agent.activate()
 
         # now wait ...
